a21f05f01007cb4a05e6adf4d110df049b323659
Nathan Sweet <nathan.j.sweet852@gmail.com>
2023-11-26 14:20:28 -0800

    added comment instructions to setup.js

---
+++ setup.js
@@ -0,0 +1,25 @@
+
+//it there s a database
+//make your server user
+
+//droptables,create empty tables
+//DROP TABLES IF EXISTS users,worlds;
+
+/*for creation                    
+CREATE TABLE worlds (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    worldName VARCHAR(50) NOT NULL,
+    ownerId INT NOT NULL,
+    img1Id INT,
+    img2Id INT,
+    mainPage BLOB,
+    pages BLOB,
+    navNames BLOB,
+    navItems BLOB
+);
+*/
+// CREATE TABLE users (
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     username VARCHAR(50) NOT NULL,
+//     password VARCHAR(255) NOT NULL
+// )
