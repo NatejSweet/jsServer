3df5d6228b4991f92c8d55f96fbb3ede2d447754
Tristan H <tristanskyhook@gmail.com>
2023-11-26 16:15:18 -0800

    WORKS BITCH

--- README.md
+++ README.md
@@ -2,17 +2,19 @@
 npm i to install dependencies
 npm run setup to setup database
 npm run start to start server
-and npm run dl to install mariadb if on ubuntu
 
-## insatlling mariadb on ubuntu
+## insatlling mariadb and create database
 sudo apt install mairadb 
-sudo mysql.server start
-mariadb
-ur in the mariadb shell now
-create a database for everything - CREATE DATABASE databaseName
-access sql database - USE mysql;
-create a user in this database - INSERT into users 
+sudo mariadb
+CREATE DATABASE databaseName
 
 CREATE USER 'user1'@localhost IDENTIFIED BY 'password1';
-
 GRANT ALL PRIVILEGES ON *.* TO 'user1'@localhost IDENTIFIED BY 'password1';
+
+then create a .env matching
+
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=saves
+DB_USER=user1
+DB_PASSWORD=password1
--- setup.js
+++ setup.js
@@ -27,12 +27,12 @@ async function dropTables(){
         const result = await conn.query(
         'DROP TABLES IF EXISTS users,worlds');
         console.log(result);
+        if (conn) conn.end();
         // Authentication failed
     } catch (err) {
         console.log(err);
         res.status(500).send('drop table failed');
     }
-    if (conn) conn.end();
 }
 async function createTables(){
     try {
@@ -44,19 +44,19 @@ async function createTables(){
     } catch (err) {
         console.log(err);
         res.status(500).send('create table failed');
+        if (conn) conn.end();
     }
-    if (conn) conn.end();
     try {
         let conn = await pool.getConnection();
         const result = await conn.query(
         'CREATE TABLE worlds (id INT AUTO_INCREMENT PRIMARY KEY,worldName VARCHAR(50) NOT NULL,ownerId INT NOT NULL,img1Id INT,img2Id INT,mainPage BLOB,pages BLOB,navNames BLOB,navItems BLOB)');
         console.log(result);
+        if (conn) conn.end();
         // Authentication failed
     } catch (err) {
         console.log(err);
         res.status(500).send('create table failed');
     }
-    if (conn) conn.end();
 }
 dropTables();
 createTables();
