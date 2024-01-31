b04e3d425589bda2a89f2e1f7d8ca886213aacd1
Nathan Sweet <nathan.j.sweet852@gmail.com>
2024-01-31 13:51:23 -0800

    started work on embedded links

--- public/viewing/javascript/viewMainPage.js
+++ public/viewing/javascript/viewMainPage.js
@@ -79,7 +79,7 @@ function fillMainContent(mainPage, pageName) {
     titleDiv.setAttribute("class", "titleDiv");
     let titleText = document.createElement("h2");
     titleText.setAttribute("class", "titleText");
-    titleText.appendChild(document.createTextNode(title[0]));
+    titleText.innerHTML = linkify(title[0]);
     titleDiv.appendChild(titleText);
     title[1].forEach((subTitle) => {
       let subTitleDiv = document.createElement("div");
@@ -261,4 +261,32 @@ function updateMap(imgId) {
     .then((content) => {
       img.setAttribute("src", content.src);
     });
+}
+
+function linkify(inputText) {
+  var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+  //URLs starting with http://, https://, or ftp://
+  replacePattern1 =
+    /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+  replacedText = inputText.replace(
+    replacePattern1,
+    '<a href="$1" target="_blank">$1</a>'
+  );
+
+  //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+  replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+  replacedText = replacedText.replace(
+    replacePattern2,
+    '$1<a href="http://$2" target="_blank">$2</a>'
+  );
+
+  //Change email addresses to mailto:: links.
+  replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
+  replacedText = replacedText.replace(
+    replacePattern3,
+    '<a href="mailto:$1">$1</a>'
+  );
+
+  return replacedText;
 }
